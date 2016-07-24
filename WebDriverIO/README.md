31b5bd92d4f7616acfbd5332d23b64972fe31159
Daniel4x <Daniel4x@gmail.com>
2016-07-24 09:08:32 +0300

    Added WebDriverIO Jeremy's sample from the community :)

---
+++ WebDriverIO/README.md
@@ -0,0 +1,6 @@
+##WebDriverIO
+
+WebdriverIO is a NodeJS based WebDriver which let's you control your browser or a mobile application.
+**Read more about WebDriverIO**
+WebDriverIO at [Perfecto community](https://community.perfectomobile.com/posts/1097783-webdriver-io-connecting-to-cloud)
+WebDriverIO [official site](http://webdriver.io/)
---
+++ WebDriverIO/WebDriverIO.js
@@ -0,0 +1,22 @@
+var webdriverio = require('webdriverio');
+var options = {
+        desiredCapabilities: {
+             platformName: 'ANDROID',                       
+             browserName: 'mobileOS',                                             
+             deviceName: 'yourDeviceID',
+             user: 'yourEmail@perfectomobile.com',
+             password:'yourPassword',
+    },
+    host: 'yourHost.perfectomobile.com',
+    path: '/nexperience/perfectomobile/wd/hub',
+port:80 
+};
+ 
+webdriverio
+    .remote(options)
+    .init()
+    .url('http://www.google.com')
+    .getTitle().then(function(title) {
+        console.log('Title was: ' + title);
+    })
+    .end();
