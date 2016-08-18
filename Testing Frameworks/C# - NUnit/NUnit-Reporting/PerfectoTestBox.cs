27b40ff072ba878eddc75d7d66f295f26476adc8
Daniel4x <Daniel4x@gmail.com>
2016-08-18 10:03:11 +0300

    Added NUnit templates

---
+++ Testing Frameworks/C# - NUnit/NUnit-Reporting/PerfectoTestBox.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Remote;
+
+namespace ReportingTests.NUnit
+{
+    /// <summary>
+    /// Test base class
+    /// </summary>
+    /// <remarks> 
+    /// Create RemoteWebDriverExtended and Reportium client object.
+    /// Configure here what happensed before and after each test and
+    /// before and after all tests.
+    /// </remarks>
+    [TestFixture]
+    class PerfectoTestBox
+    {
+        //Perfecto lab username, password and host.
+        private const string PERFECTO_USER = "MY_USER";
+        private const string PERFECTO_PASS = "MY_PASS";
+        private const string PERFECTO_HOST = "MY_HOST.perfectomobile.com";
+
+        internal static RemoteWebDriverExtended driver;
+
+        /// <summary>
+        /// Setup once what happens before all tests
+        /// </summary>
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            //DesiredCapabilities capabilities = new DesiredCapabilities(browserName, string.Empty, new Platform(PlatformType.Any));
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+
+            //Provide your Perfecto lab user and pass
+            capabilities.SetCapability("user", PERFECTO_USER);
+            capabilities.SetCapability("password", PERFECTO_PASS);
+
+            //Device capabilities
+            capabilities.SetCapability("platformName", "Android");
+
+            //Create RemoteWebDriver
+            var url = new Uri(string.Format("http://{0}/nexperience/perfectomobile/wd/hub", PERFECTO_HOST));
+            driver = new RemoteWebDriverExtended(new HttpAuthenticatedCommandExecutor(url), capabilities);
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
+
+        }
+
+        /// <summary>
+        /// Class cleanup (after all tests)
+        /// </summary>
+        /// <remarks> 
+        /// This method runs after all the tests in the class are finished.
+        /// </remarks>
+        [OneTimeTearDown]
+        public static void tearDown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+    }
+}
---
+++ Testing Frameworks/C# - NUnit/README.md
@@ -0,0 +1,26 @@
+## NUnit
+
+This project demonstrates using NUnit testing framework for C#.
+
+The project seperated into two files the first file: [PerfectoTestBox.cs](PerfectoTestBox.cs) which determines the test configuration.
+At this file you will find the following properties:<br/>
+`[OneTimeSetUp]` - Run before all the tests in the class.<br/>
+`[SetUp]` - Run before each test. <br/>
+`[TearDown]` - Run after each test.<br/>
+`[OneTimeTearDown]` - Run after all tests completed.
+
+The second file [MyTestClass.cs](MyTestClass.cs) include the tests : <br/>
+The first test navigates to google and searches for the PerfectoCode repository in GitHub.<br/>
+Then clicks on the first search result and asserts that the Keyword *Perfecto* exists in the title of the page. 
+
+**TODO:**
+- Make sure you have installed Perfecto [plugin for Visual Studio](https://www.perfectomobile.com/integrations/continuous-quality-integrated-visual-studio).
+- Download the project and import the .sln file to Visual Studio IDE.
+- Set your Perfecto lab User, Password and Host in the [PerfectoTestBox.cs](PerfectoTestBox.cs) file under *TestFixture* annotation.
+```Csharp
+  const string PERFECTO_USER = "MY_USER";
+  const string PERFECTO_PASS = "MY_PASS";
+  const string PERFECTO_HOST = "MY_HOST.perfectomobile.com";
+```
+- Write your tests and add more tests methods with `[TestFixture]` annotation. 
+- Run the tests as [NUnit tests](https://www.nuget.org/packages/NUnit/) with the [test explorer](https://msdn.microsoft.com/en-us/library/hh270865.aspx) in Visual Studio.
